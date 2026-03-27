4fe3a27a4fde2a7b792653608d3a898762740df8
tenshinohane <ikuzosentai.poohrengyer@gmail.com>
2026-03-27 13:31:58 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://your-railway-url.up.railway.app";
+const API_BASE_URL = "https://brawl-api-production.up.railway.app";
 
 const searchBtn = document.getElementById("searchBtn");
 const playerTagInput = document.getElementById("playerTag");
